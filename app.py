a52e09f30f9cd6ba1a2d23909939051e9ee523bb
Osheen Langer <87273526+OL10@users.noreply.github.com>
2026-02-17 02:18:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -410,12 +410,11 @@ with col1:
     
     movie_title = selected_movie.split(' (')[0]
     
-    # n_recommendations = st.slider(
-    #     "Number of recommendations:",
-    #     min_value=3,
-    #     max_value=10,
-    #     value=5
-    # )
+    n_recommendations = st.slider(
+        "Number of recommendations:",
+    
+        value=5
+    )
     
     get_recs = st.button("Get Recommendations", type="primary")
 
